4ef71d9e2901d7de77ae56e1973667a4f150b85c
Brandon Green <brandon@brandonleeproductions.com>
2020-02-15 03:20:47 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -31,9 +31,9 @@ def play
 end
 
 def list(songs)
-  songs.each_with_index {|song, index|}
-    puts "#{index} + 1. #{song}"
-    }
+songs.each_with_index {|song, index|
+  puts "#{index+1}. #{song}"
+}
 end
 
 def exit_jukebox
