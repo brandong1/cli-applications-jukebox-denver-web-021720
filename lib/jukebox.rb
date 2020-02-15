60ed000f87528fc17e83bac03175eada89c9532d
Brandon Green <brandon@brandonleeproductions.com>
2020-02-15 03:11:41 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,32 @@
-# Add your code here
+# def say_hello
+#   "Hi #{name}!"
+# end
+
+# puts "Enter your name: "
+# users_name = gets.strip
+
+# puts say_hello(users_name)
+
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+def help 
+end
+
+def play
+end
+
+def list 
+end
+
+def exit_jukebox
+end
